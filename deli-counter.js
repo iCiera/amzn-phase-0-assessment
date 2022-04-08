3948b58222c0818cc323a7463cf31e9b8dc48dab
ciera <cieranicole.m15@gmail.com>
2022-04-07 21:06:54 -0400

    done

--- deli-counter.js
+++ deli-counter.js
@@ -1,24 +1,49 @@
 // 1. Write your functions here
 
+function line(arr){
+  if(arr.length === 0) {
+    return 'The line is currently empty'
+  }
+    let arrToString = '' 
+    for(let i = 0; i < arr.length; i++) {
+      arrToString += `${i+1}. ${arr[i]}, ` 
+    }
+  return `The line is currently: ` + arrToString.slice(0, -2)
+  }
+  
+  function takeANumber(arr, name) {
+    arr.push(name);
+    return `Welcome, ${name}. You are number ${arr.indexOf(name) + 1} in line.`
+  };
+
+  function nowServing(arr) {
+
+    if (arr.length == 0) {
+      return 'There is nobody waiting to be served!'
+    }
+    let current = arr[0]
+    arr.shift()
+    return `Currently serving ${current}. `
+  }
 
 // 2. Example Usage
 
-// const katzDeli = []
+const katzDeli = []
 
-// takeANumber(katzDeli, "Ada") //=> Welcome, Ada. You are number 1 in line.
-// takeANumber(katzDeli, "Grace") //=> Welcome, Grace. You are number 2 in line.
-// takeANumber(katzDeli, "Kent") //=> Welcome, Kent. You are number 3 in line.
+ //console.log(takeANumber(katzDeli, "Ada")) //=> Welcome, Ada. You are number 1 in line.
+ //console.log(takeANumber(katzDeli, "Grace")) //=> Welcome, Grace. You are number 2 in line.
+ //console.log(takeANumber(katzDeli, "Kent")) //=> Welcome, Kent. You are number 3 in line.
 
-// line(katzDeli) //=> "The line is currently: 1. Ada 2. Grace 3. Kent"
+ //console.log(line(katzDeli)) //=> "The line is currently: 1. Ada 2. Grace 3. Kent"
 
-// nowServing(katzDeli) //=> "Currently serving Ada."
+ //console.log(nowServing(katzDeli)) //=> "Currently serving Ada."
 
-// line(katzDeli) //=> "The line is currently: 1. Grace 2. Kent"
+ //console.log(line(katzDeli)) //=> "The line is currently: 1. Grace 2. Kent"
 
-// takeANumber(katzDeli, "Matz") //=> Welcome, Matz. You are number 3 in line.
+ //console.log(takeANumber(katzDeli, "Matz")) //=> Welcome, Matz. You are number 3 in line.
 
-// line(katzDeli) //=> "The line is currently: 1. Grace 2. Kent 3. Matz"
+ //console.log(line(katzDeli)) //=> "The line is currently: 1. Grace 2. Kent 3. Matz"
 
-// nowServing(katzDeli) //=> "Currently serving Grace."
+ //console.log(nowServing(katzDeli)) //=> "Currently serving Grace."
 
-// line(katzDeli) //=> "The line is currently: 1. Kent 2. Matz"
+ //console.log(line(katzDeli)) //=> "The line is currently: 1. Kent 2. Matz"
